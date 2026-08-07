1edd68fb30767a3ee46875d7d7572bac498d1e7d
vharelabdissalam123 <salamvharelabdi@gmail.com>
2026-08-07 08:24:10 +0700

    Create setup_users.sql

---
+++ datasederhana/setup_users.sql
@@ -0,0 +1,47 @@
+<?php
+session_start();
+include "koneksi.php";
+
+$error = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $query = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+    $user  = mysqli_fetch_array($query);
+
+    if ($user && $password === $user['password']) {
+        $_SESSION['username'] = $user['username'];
+        header("Location: index.php");
+        exit;
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>Login</h2>
+
+<?php if ($error) { ?>
+    <p style="color:red;"><?= $error; ?></p>
+<?php } ?>
+
+<form action="login.php" method="POST">
+    Username <br>
+    <input type="text" name="username"><br><br>
+
+    Password <br>
+    <input type="password" name="password"><br><br>
+
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
