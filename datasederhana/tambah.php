e0bc1cb4150d8e545c56c3a9fb7433f0c66d235c
vharelabdissalam123 <salamvharelabdi@gmail.com>
2026-08-14 11:44:44 +0700

    Update tambah.php

--- datasederhana/tambah.php
+++ datasederhana/tambah.php
@@ -1,5 +1,12 @@
 <?php
+session_start();
 include "koneksi.php";
+
+// hanya admin yang boleh menambah data
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
+    header("Location: index.php");
+    exit;
+}
 
 $nisn  = $_POST['nisn'];
 $nama  = $_POST['nama'];
